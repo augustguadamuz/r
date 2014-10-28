eb8add1ba2581919daa4db0f642c7ecd644f8f05
August Guadamuz <augustguadamuz@gmail.com>
2014-10-27 19:56:40 -0700

    added initial code from sample code in the book

--- doing_data_science_code.R
+++ doing_data_science_code.R
@@ -16,6 +16,23 @@ head(data1)
 # Create a new variable, age_group, that categorizes users as
 # "<18", "18-24", "25-34", "35-44", "45-54", "55-64", and "65+"
 
+data1$agecat <- cut(data1$Age,c(-Inf,0,18,24,34,44,54,64,Inf))
+?cut # convert numberic to factor
+
+head(data1$agecat)
+summary(data1)
+
+#brackets
+
+install.packages("doBy")
+library("doBy")
+siterange <- function(x){c(length(x), min(x), mean(x), max(x))}
+summaryBy(Age~agecat, data = data1, FUN = siterange)
+
+# so only signed in users have ages ad genders
+
+summaryBy(Gender+Signed_In+Impressions+Clicks~agecat, data = data1)
+
 
 # Plot the distributions of number impressions and CTR for the 6 age categories
 
